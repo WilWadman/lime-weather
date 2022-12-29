cc0d318a3615355b9cc133f2ca7abf07c60ea46b
William Wadman <wadmanwill@gmail.com>
2022-12-29 09:48:30 -0500

    Add minor updates to html structure

--- express_server.js
+++ express_server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.listen(8080, () => {
+  console.log('App listening on port 8080');
 });
