6a167a08bf16eacf08bcf4c265f23ff356e99b51
William Wadman <wadmanwill@gmail.com>
2022-12-30 14:45:25 -0500

    Add dotenv, env file and hide API key from github/public

--- express_server.js
+++ express_server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const dotenv = require('dotenv');
 
 const bodyParser = require('body-parser');
 const axios = require('axios');
+
+// dotenv used to hide openweather API key
+dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,11 +19,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/submit', (req, res) => {
+  const apiKey = process.env.API_KEY;
   const location = req.body.location;
-  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=4361ec255b6430531d31af0eb6354330`;
-
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+console.log(process.env)
   let data1;
-  // using axios to perform the first api pull and declaring the vairables for the second api pull in the promise for the second api pull
+  // using axios to perform the first api pull and declaring the variables for the second api pull to be used in the promise for the second api pull
   axios.get(apiURL)
   .then((response1) => {
     data1 = response1.data;
@@ -28,7 +33,7 @@ app.post('/submit', (req, res) => {
     const stateCode = ''; 
     const countryCode = data1.sys.country;
     const limit = ''; 
-    const apiURL2 =`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=4361ec255b6430531d31af0eb6354330`
+    const apiURL2 =`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=${apiKey}`
     return axios.get(apiURL2);
   })
   // using a second promise with the data from apiURL2 pull to get coords and rendering data1 and data2 for the html
