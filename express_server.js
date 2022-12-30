800f46cbbcb1760b016f888bf2079a1594a978df
William Wadman <wadmanwill@gmail.com>
2022-12-30 18:23:23 -0500

    Remove comments, fix indentation, add additonal comemnts, convert error handleing to on page

--- express_server.js
+++ express_server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 // dotenv used to hide openweather API key
 dotenv.config();
@@ -13,47 +13,45 @@ dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //include express static to access CSS file
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.post('/submit', (req, res) => {
+app.post("/submit", (req, res) => {
   const apiKey = process.env.API_KEY;
   const location = req.body.location;
   const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
-console.log(process.env)
+  console.log(process.env);
   let data1;
   // using axios to perform the first api pull and declaring the variables for the second api pull to be used in the promise for the second api pull
-  axios.get(apiURL)
-  .then((response1) => {
-    data1 = response1.data;
-    console.log("DATA1", data1);
-    const cityName = data1.name;
-    const stateCode = ''; 
-    const countryCode = data1.sys.country;
-    const limit = ''; 
-    const apiURL2 =`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=${apiKey}`
-    return axios.get(apiURL2);
-  })
-  // using a second promise with the data from apiURL2 pull to get coords and rendering data1 and data2 for the html
-  .then((response2) =>{
-    const data2 = response2.data;
-    console.log("DATA2", data2)
-    res.render('index', { data1: data1, data2: data2 });
-    return;
-  })
+  axios
+    .get(apiURL)
+    .then((response1) => {
+      data1 = response1.data;
+      const cityName = data1.name;
+      const stateCode = "";
+      const countryCode = data1.sys.country;
+      const limit = "";
+      const apiURL2 = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=${apiKey}`;
+      return axios.get(apiURL2);
+    })
+    // using a second promise with the data from apiURL2 pull to get coords and rendering data1 and data2 for the html
+    .then((response2) => {
+      const data2 = response2.data;
+      res.render("index", { data1: data1, data2: data2 });
+      return;
+    })
 
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
-      res.status(500).send('Your location was blank or does not exist, please try again! <a href="/">Return to homepage</a>');
+      res.render("index", { errorMessage: 'Your location was blank or does not exist, please try again!' });
     });
 });
 
-
 app.listen(8080, () => {
-  console.log('App listening on port 8080');
+  console.log("App listening on port 8080");
 });
