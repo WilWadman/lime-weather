1ed29d577db07f54258096edcfc72b9a8aedddd7
William Wadman <wadmanwill@gmail.com>
2022-12-30 14:04:08 -0500

    Add both API calls this commit includes an API call for both the current weather and the GEO coords, the first API pull also sets the variables for the second API call to successfully get the GPS coords and display them on the app

--- express_server.js
+++ express_server.js
@@ -15,22 +15,35 @@ app.get('/', (req, res) => {
 });
 
 app.post('/submit', (req, res) => {
-  console.log("BODY2",req.body.location)
   const location = req.body.location;
-  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=4361ec255b6430531d31af0eb6354330`
-  
-  
- 
-axios.get(apiURL)
-  .then(res => res.data)
-  .then(data => {
-    // parse the response and display the relevant information 
-    console.log(data);
-  })
-  .catch(error => console.error(error));
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=4361ec255b6430531d31af0eb6354330`;
 
-}
-);
+  let data1;
+  // using axios to perform the first api pull and declaring the vairables for the second api pull in the promise for the second api pull
+  axios.get(apiURL)
+  .then((response1) => {
+    data1 = response1.data;
+    console.log("DATA1", data1);
+    const cityName = data1.name;
+    const stateCode = ''; 
+    const countryCode = data1.sys.country;
+    const limit = ''; 
+    const apiURL2 =`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=4361ec255b6430531d31af0eb6354330`
+    return axios.get(apiURL2);
+  })
+  // using a second promise with the data from apiURL2 pull to get coords and rendering data1 and data2 for the html
+  .then((response2) =>{
+    const data2 = response2.data;
+    console.log("DATA2", data2)
+    res.render('index', { data1: data1, data2: data2 });
+    return;
+  })
+
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Your location was blank or does not exist, please try again! <a href="/">Return to homepage</a>');
+    });
+});
 
 
 app.listen(8080, () => {
